e2645f7b4eff00f661f098a17d750c22807b417d
alxayeed <alxayeed@gmail.com>
2023-07-23 09:43:02 +0600

    project initialized

---
+++ swift_todo/swift_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_todoApp.swift
+//  swift_todo
+//
+//  Created by Bizzntek Ltd on 23/7/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
