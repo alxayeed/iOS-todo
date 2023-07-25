68be2479d6e81ac8204687bca9629344e522aea4
alxayeed <alxayeed@gmail.com>
2023-07-25 17:22:16 +0600

    Add: profile screen UI created. Log out feature added

--- swift_todo/ViewModels/ProfileViewViewModel.swift
+++ swift_todo/ViewModels/ProfileViewViewModel.swift
@@ -4,5 +4,18 @@
 //
 //  Created by alxayeed on 23/7/23.
 //
-
+import FirebaseAuth
 import Foundation
+
+
+class ProfileViewViewModel: ObservableObject{
+    
+    
+    func logout(){
+        do {
+            try Auth.auth().signOut()
+        } catch{
+            print(error)
+        }
+    }
+}
--- swift_todo/Views/ProfileView.swift
+++ swift_todo/Views/ProfileView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    
     var body: some View {
         NavigationView{
-            VStack{}
+            VStack{
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 125, height: 125)
+                
+                VStack(alignment: .leading){
+                    HStack{
+                        Text("Name: ")
+                        Text("Al Sayeed")
+                    }.padding(.top, 15)
+                    HStack{
+                        Text("Email: ")
+                        Text("Al Sayeed")
+                    }.padding(.top,5)
+                    HStack{
+                        Text("Member since: ")
+                        Text("Al Sayeed")
+                    }.padding(.top,5)
+                }
+                Button{
+                    viewModel.logout()
+                } label: {
+                    Text("Log out")
+                }
+                .foregroundColor(Color.red)
+                .padding()
+            
+                Spacer()
+            }
                 .navigationTitle("Profile")
         }
     }
